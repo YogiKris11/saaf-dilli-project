8b0beddee40b5574eef45ddd43b41d900e0343bb
YogiKris11 <yogeshkrishna2018@gmail.com>
2025-10-04 23:02:05 +0530

    Remove build and node_modules from Git tracking

--- .next/static/chunks/src_app_login_page_tsx_09c437d9._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_454eedaf._.js",
-  "static/chunks/src_c77ee70d._.js"
-],
-    source: "dynamic"
-});
